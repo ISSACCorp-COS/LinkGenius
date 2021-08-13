6625c6fa807d482167881db155cce603a2c5e33c
Tom Henry <tom.henry@issaccorp.com>
2021-08-13 15:01:09 -0600

    Try just doing a link.

--- proxy_frame/iframe.py
+++ proxy_frame/iframe.py
@@ -1,16 +1,29 @@
 def create_iframe(port, height):
+    # shell = """
+    #     (async () => {
+    #         const url = await google.colab.kernel.proxyPort(%PORT%, {"cache": true});
+    #         console.log(`Adding iframe from URL:${url}`);
+    #         const iframe = document.createElement('iframe');
+    #         iframe.src = url;
+    #         iframe.setAttribute('width', '100%');
+    #         iframe.setAttribute('height', '%HEIGHT%');
+    #         iframe.setAttribute('frameborder', 0);
+    #         document.body.appendChild(iframe);
+    #     })();
+    # """
     shell = """
         (async () => {
             const url = await google.colab.kernel.proxyPort(%PORT%, {"cache": true});
             console.log(`Adding iframe from URL:${url}`);
-            const iframe = document.createElement('iframe');
-            iframe.src = url;
-            iframe.setAttribute('width', '100%');
-            iframe.setAttribute('height', '%HEIGHT%');
-            iframe.setAttribute('frameborder', 0);
-            document.body.appendChild(iframe);
+            const link = document.createElement('a');
+            link.href = url;
+            link.rel = "nofollow";
+            link.target = "_blank";
+            link.appendChild(url);
+            document.body.appendChild(link);
         })();
     """
+    target="_parent">
     replacements = [
         ("%PORT%", "%d" % port),
         ("%HEIGHT%", "%d" % height),
