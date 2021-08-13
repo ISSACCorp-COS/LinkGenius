05dc79b3b8c3ac682fed105a5d219fdec8f678f5
Tom Henry <tom.henry@issaccorp.com>
2021-08-13 12:46:14 -0600

    Implement display_iframe

--- proxy_frame/iframe.py
+++ proxy_frame/iframe.py
@@ -1,2 +1,29 @@
+import IPython.display
+
+def display_iframe(port, height):
+    shell = """
+        (async () => {
+            const url = await google.colab.kernel.proxyPort(%PORT%, {"cache": true});
+            console.log(`Adding ifram from URL:${url}`);
+            const iframe = document.createElement('iframe');
+            iframe.src = url;
+            iframe.setAttribute('width', '100%');
+            iframe.setAttribute('height', '%HEIGHT%');
+            iframe.setAttribute('frameborder', 0);
+            document.body.appendChild(iframe);
+        })();
+    """
+    replacements = [
+        ("%PORT%", "%d" % port),
+        ("%HEIGHT%", "%d" % height),
+    ]
+    for (k, v) in replacements:
+        shell = shell.replace(k, v)
+
+    script = IPython.display.Javascript(shell)
+    IPython.display.display(script)
+
+    return "Hello from python!"
+
 def show():
-    print("Showing from show in iframe")
+    return display_iframe(3771, 500)
